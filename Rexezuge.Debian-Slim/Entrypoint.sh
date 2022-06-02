479bf9eec8e5c4ff24a6225df1cef7788c471e5d
git.Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2022-06-02 10:29:22 -0400

    FEAT: Only Write Default Server Config if File Not Exist

--- Rexezuge.Debian-Slim/Entrypoint.sh
+++ Rexezuge.Debian-Slim/Entrypoint.sh
@@ -3,14 +3,20 @@
 # Update Game
 SteamCMD/steamcmd.sh +force_install_dir ../L4D2Content +login anonymous +app_update 222860 +quit
 
-# Server Config
-cat > L4D2Content/left4dead2/cfg/server.cfg <<EOF
+# Write Server Config
+if [ -f "L4D2Content/left4dead2/cfg/server.cfg" ]
+then
+  echo "Server Config Already Exist"
+else
+  echo "Writing Default Server Config"
+  cat > L4D2Content/left4dead2/cfg/server.cfg << EOF
 hostname "${HOSTNAME}"
 motd_enabled 0
 sv_region ${REGION}
 sv_logecho 1
 sv_steamgroup ${STEAMGROUP}
 EOF
+fi
 
 # Start Game
 if [ "$SECURE" = 1 ]
