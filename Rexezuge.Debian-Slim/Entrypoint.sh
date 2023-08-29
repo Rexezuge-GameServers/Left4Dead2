4993a8bdbf87c644effe07e703130848ff0243fc
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2023-08-29 19:09:14 -0400

    FIX: Simplify Start Command

--- Rexezuge.Debian-Slim/Entrypoint.sh
+++ Rexezuge.Debian-Slim/Entrypoint.sh
@@ -19,11 +19,11 @@ EOF
 fi
 
 # Start Game
-if [ "$SRV_SECURESERVER" = 1 ]
-then
-echo "Starting VAC Secured Server"
-/L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -secure -noipx
-else
-echo "Starting Unsecured Server"
-/L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -insecure -noipx
-fi
+/L4D2Content/srcds_run \
+  -console \
+  -game left4dead2 \
+  -port "$SRV_PORT" \
+  +maxplayers "$SRV_PLAYERS" \
+  +map "$SRV_MAP" \
+  $( [ "$SRV_SECURESERVER" = 1 ] && echo "-secure" || echo "-insecure" ) \
+  -noipx
