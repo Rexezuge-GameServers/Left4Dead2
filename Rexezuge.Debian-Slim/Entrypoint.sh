decc0b75822428622137e3e665f7528692fbf0b0
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2023-08-29 18:09:06 -0400

    BREAKING CHANGE: Use Regular User Instead of `root`

--- Rexezuge.Debian-Slim/Entrypoint.sh
+++ Rexezuge.Debian-Slim/Entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Update Game
-SteamCMD/steamcmd.sh +force_install_dir ../L4D2Content +login anonymous +app_update 222860 +quit
+/SteamCMD/steamcmd.sh +force_install_dir ../L4D2Content +login anonymous +app_update 222860 +quit
 
 # Write Server Config
 if [ "$CFG_FORCEOVERWRITE" = 1 ]
 then
-cat > L4D2Content/left4dead2/cfg/server.cfg << EOF
+cat > /L4D2Content/left4dead2/cfg/server.cfg << EOF
 hostname "${CFG_HOSTNAME}"
 motd_enabled 0
 sv_logecho 1
@@ -22,8 +22,8 @@ fi
 if [ "$SRV_SECURESERVER" = 1 ]
 then
 echo "Starting VAC Secured Server"
-L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -secure -noipx
+/L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -secure -noipx
 else
 echo "Starting Unsecured Server"
-L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -insecure -noipx
+/L4D2Content/srcds_run -console -game left4dead2 -port "$SRV_PORT" +maxplayers "$SRV_PLAYERS" +map "$SRV_MAP" -insecure -noipx
 fi
