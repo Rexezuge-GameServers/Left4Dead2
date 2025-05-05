58f0f7223d67f84f7907851fe9d574fe12e9de17
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2025-05-04 22:10:41 -0400

    FEAT: optimize default server config

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -37,27 +37,32 @@ fi
 if [ "$CFG_RESTORE_DEFAULT" = 1 ]
 then
 cat > /L4D2Content/left4dead2/cfg/server.cfg << EOF
-// Information
+// Server Info
 hostname "${CFG_INFORMATION_HOSTNAME}"
 sv_region ${CFG_INFORMATION_REGION}
 sv_steamgroup ${CFG_INFORMATION_STEAM_GROUP}
 motd_enabled 0
 
-// Settings
+// Game Settings
 sv_gametypes "${CFG_SETTINGS_GAME_TYPE}"
 sv_consistency 0
 sv_voiceenable 0
+sv_pausable 0
+director_no_human_zombies 1
+sv_versus_swapteams 1
 
 // Logging
 sv_logecho 1
 sv_logfile 0
 
-// Network Tweaks
-rate 10000
-sv_minrate 5000
+// Network Performance
+rate 30000
+sv_minrate 10000
 sv_maxrate 30000
-sv_mincmdrate 20
-sv_maxcmdrate 33
+sv_mincmdrate 30
+sv_maxcmdrate 66
+sv_minupdaterate 30
+sv_maxupdaterate 66
 
 // Security
 sv_cheats 0
@@ -65,6 +70,11 @@ sv_lan 0
 sv_allow_lobby_connect_only 0
 sv_allow_wait_command 0
 
+// Stability and Performance
+sv_timeout 60
+sv_maxplayers 8
+sv_forcepreload 1
+fps_max 0
 EOF
 fi
 
